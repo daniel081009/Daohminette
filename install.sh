424b8721925342461ce90e06dea929359f4b2ee4
DAN <58280710+daniel081009@users.noreply.github.com>
2022-01-19 19:12:05 +0900

    up

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 
-cp ./Daohminette.sh ~/Documents
+curl https://raw.githubusercontent.com/daniel081009/Daohminette/main/Daohminette.sh > ~/Documents/Daohminette.sh
 if [[ `cat ~/.zshrc` == *"daohminette"* ]];then
    echo "${green}[✔] 이미 설치가 되어 있습니다${clear}"
 else
